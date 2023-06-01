09dd66203a8cc9e07a4a1e873e69935e850351d8
lowright09 <lowright09@gmail.com>
2023-06-01 00:34:13 -0500

    update

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,6 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by LaunchCode
@@ -11,11 +14,18 @@ import org.junit.runners.JUnit4;
 public class JobTest {
 @Test
     public void testSettingJobId(){
-
+    Job job1 = new Job();
+    Job job2 = new Job();
+    //assertNotEquals(unexpected , actual );
     }
 
 @Test
     public void testJobConstructorSetsAllFields(){
+Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+}
+
+@Test
+    public  void testJobsForEquality(){
 
 }
 

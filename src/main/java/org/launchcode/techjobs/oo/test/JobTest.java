62cddab11c093f000a0ecf277a03ff4c0df75b05
lowright09 <lowright09@gmail.com>
2023-06-01 19:41:56 -0500

    update on tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 /**
@@ -12,21 +13,52 @@ import static org.junit.Assert.assertNotEquals;
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
 @Test
     public void testSettingJobId(){
-    Job job1 = new Job();
+    Job job = new Job();
     Job job2 = new Job();
-    //assertNotEquals(unexpected , actual );
+    assertNotEquals(job.getId() , job2.getId() );
     }
 
 @Test
     public void testJobConstructorSetsAllFields(){
 Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-}
+    assertEquals("Product tester", job.getName());
+    assertEquals(new Employer("ACME").getValue(), job.getEmployer().getValue());
+    Location location = new Location();
+    assertEquals(new Location("Desert").getValue(), job.getLocation().getValue());
+    PositionType positionType = new PositionType();
+    assertEquals(new PositionType("Quality control").getValue(), job.getPositionType().getValue());
+    CoreCompetency coreCompetency = new CoreCompetency();
+    assertEquals(new CoreCompetency("Persistence").getValue(), job.getCoreCompetency().getValue());
+
+ }
 
 @Test
     public  void testJobsForEquality(){
+    Job job = new Job();
+    Job job1 = new Job();
+    assertNotEquals(job.getId() , job1.getId());
+ }
+@Test
+    public void testToStringStartsAndEndsWithNewLine(){
 
-}
+ }
+
+@Test
+    public void testToStringContainsCorrectLabelsAndData(){
+
+ }
+
+ @Test
+    public void testToStringHandlesEmptyField(){
+
+  }
+
+
+
+
+
 
 }

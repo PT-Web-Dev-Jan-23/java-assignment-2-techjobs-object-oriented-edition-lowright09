4ce3dfb97942f91708e0d044b280a8a711904c23
lowright09 <lowright09@gmail.com>
2023-06-05 18:39:57 -0500

    NewRepositroryPush

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -5,8 +5,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
@@ -23,13 +23,18 @@ public class JobTest {
 
 @Test
     public void testJobConstructorSetsAllFields(){
-Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    assertTrue(job.getId() > 0);
     assertEquals("Product tester", job.getName());
+    assertTrue(job instanceof Job);
     assertEquals(new Employer("ACME").getValue(), job.getEmployer().getValue());
+    assertTrue(job.getLocation() instanceof Location   );
     Location location = new Location();
     assertEquals(new Location("Desert").getValue(), job.getLocation().getValue());
+    assertTrue(job.getPositionType() instanceof  PositionType);
     PositionType positionType = new PositionType();
     assertEquals(new PositionType("Quality control").getValue(), job.getPositionType().getValue());
+    assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
     CoreCompetency coreCompetency = new CoreCompetency();
     assertEquals(new CoreCompetency("Persistence").getValue(), job.getCoreCompetency().getValue());
 
@@ -37,9 +42,9 @@ Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert")
 
 @Test
     public  void testJobsForEquality(){
-    Job job = new Job();
     Job job1 = new Job();
-    assertNotEquals(job.getId() , job1.getId());
+    Job job2 = new Job();
+    assertFalse(job1.equals(job2));
  }
 @Test
     public void testToStringStartsAndEndsWithNewLine(){
